003a4b558bb3ce4dd4010120b5bfca927172a05c
Vika Kovalenko <kvvendetta@gmail.com>
2017-08-17 22:24:35 +0300

    Исправление ошибки слайдера.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 $('.slider').bxSlider();
-$('.popup__slider').bxSlider();
 
-
-var methodsSend = document.querySelector('.methods');
-var methodButton = document.querySelector('.methods__button');
-var buttonEmail = document.querySelector('.methods__button--email');
-var buttonMessage = document.querySelector('.methods__button--message');
-var buttonPrint = document.querySelector('.methods__button--print');
 var openPopup = document.querySelector('.list-wrapper .clean-link');
 var popup = document.querySelector('.popup');
 var closePopup = document.querySelector('.close-popup');
@@ -19,6 +12,7 @@ var closeMainPopup = function () {
 openPopup.addEventListener('click', function (e) {
     e.preventDefault();
     popup.style.display = 'block';
+    $('.popup__slider').bxSlider();
 });
 
 closePopup.addEventListener('click', function () {
@@ -30,14 +24,3 @@ document.addEventListener('keydown', function (e) {
         closeMainPopup();
     }
 });
-
-
-/*methods.addEventListener('click', function (e) {
-    var target = e.target;
-
-    if (target.classList.contains('methods__button--email')) {
-        buttonEmail.style.backgroundColor = '#00a89e';
-        buttonEmail.style.color = 'white';
-        buttonEmail.style.backgroundImage = 'url("../img-certificate/email-white.png") no-repeat 68px 16px';
-    }
-});*/
